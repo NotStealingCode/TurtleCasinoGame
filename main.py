83265c3b677591865b64ba174eb860bda561d725
NotStealingCode <105090266+NotStealingCode@users.noreply.github.com>
2022-07-30 23:46:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -17,29 +17,28 @@ while user_money > 0:
 
     if user_bet > user_money:
         print("Insufficient funds. Restart the game.")
-        isrunning = False
-    elif user_choice and user_bet:
-        isrunning = True
-        new_y_position = -160
-        for i in range(0, 6):
-            new_turtle = Turtle(shape="turtle")
-            new_turtle.color(colors[i])
-            new_turtle.penup()
-            new_turtle.goto(x=-230, y=new_y_position)
-            new_y_position += 60
-            all_turtles.append(new_turtle)
-        while isrunning:
-            for turtle in all_turtles:
-                if turtle.xcor() > 230:
-                    isrunning = False
-                    winning_turtle = turtle.pencolor()
-                    if winning_turtle == user_choice.lower():
-                        print(f"You have won! The {winning_turtle} turtle is the winner.")
-                        user_money += 2 * user_bet
-                    else:
-                        print(f"You have lost! The {winning_turtle} turtle is the winner.")
-                        user_money -= user_bet
-                    screen.clearscreen()
-                random_distance = random.randint(0, 10)
-                turtle.forward(random_distance)
+        continue
+    isrunning = True
+    new_y_position = -160
+    for i in range(0, 6):
+        new_turtle = Turtle(shape="turtle")
+        new_turtle.color(colors[i])
+        new_turtle.penup()
+        new_turtle.goto(x=-230, y=new_y_position)
+        new_y_position += 60
+        all_turtles.append(new_turtle)
+    while isrunning:
+        for turtle in all_turtles:
+            if turtle.xcor() > 230:
+                isrunning = False
+                winning_turtle = turtle.pencolor()
+                if winning_turtle == user_choice.lower():
+                    print(f"You have won! The {winning_turtle} turtle is the winner.")
+                    user_money += 2 * user_bet
+                else:
+                    print(f"You have lost! The {winning_turtle} turtle is the winner.")
+                    user_money -= user_bet
+                screen.clearscreen()
+            random_distance = random.randint(0, 10)
+            turtle.forward(random_distance)
 screen.exitonclick()
