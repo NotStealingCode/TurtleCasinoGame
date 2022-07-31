7460d258b7a136d095a429c9f6dfd266505a5863
NotStealingCode <105090266+NotStealingCode@users.noreply.github.com>
2022-07-30 23:19:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,46 @@
-from turtle import Screen
-import turtle as t
+from turtle import Turtle, Screen
 import random
 
-john = t.Turtle()
-john.speed("fastest")
-t.colormode(255)
-
-
-def random_color():
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-    random_colors = (r, g, b)
-    return random_colors
-
-size_of_gap = 0
-
-for create_shape in range(0, 72):
-    john.color(random_color())
-    john.circle(100)
-    john.setheading(size_of_gap)
-    size_of_gap += 5
+user_money = 1000
+isrunning = False
+continue_playing = True
 
 screen = Screen()
+screen.setup(width=500, height=400)
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+
+while user_money > 0:
+    all_turtles = []
+    user_choice = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color ("
+                                                                 "Red/Blue/Green/Yellow/Orange/Purple): ").lower()
+    user_bet = screen.numinput(title="Amount", prompt=f"Enter an amount to bet: (You currently have ${user_money})")
+
+    if user_bet > user_money:
+        print("Insufficient funds. Restart the game.")
+        isrunning = False
+    elif user_choice and user_bet:
+        isrunning = True
+        new_y_position = -160
+        for create_turtle in range(0, 6):
+            new_turtle = Turtle(shape="turtle")
+            new_turtle.color(colors[create_turtle])
+            new_turtle.penup()
+            new_turtle.goto(x=-230, y=new_y_position)
+            new_y_position += 60
+            all_turtles.append(new_turtle)
+        while isrunning:
+            for turtle in all_turtles:
+                if turtle.xcor() > 230:
+                    isrunning = False
+                    winning_turtle = turtle.pencolor()
+                    if winning_turtle == user_choice.lower():
+                        print(f"You have won! The {winning_turtle} turtle is the winner.")
+                        user_money += 2 * user_bet
+                        screen.clear()
+                    else:
+                        print(f"You have lost! The {winning_turtle} turtle is the winner.")
+                        user_money -= user_bet
+                        screen.clearscreen()
+                random_distance = random.randint(0, 10)
+                turtle.forward(random_distance)
 screen.exitonclick()
